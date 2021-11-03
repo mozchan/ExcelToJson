b4d30c4d4fc860a477924b47de7b9d47867e231d
mozchan <29094089+mozchan@users.noreply.github.com>
2021-11-03 12:14:32 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
 const fs = require('fs')
-let XLSX = require('xlsx')
+const XLSX = require('xlsx')
 
 const fileName = "ExcelToJson"; // ファイル名を入れる（この名前が JSON ファイル名になる）
-let workbook = XLSX.readFile(`excel/${fileName}.xlsx`)
+const workbook = XLSX.readFile(`excel/${fileName}.xlsx`)
 
 // 1．データ取得(JSON)
 let bookCategory, bookProfileDirector, bookProfileGroup
 
-workbook.SheetNames.map(sheet => { // 各シート名の変数名を指定
+workbook.SheetNames.forEach(sheet => {
+  // 各シート名の変数名を指定
   if ("項目名" == sheet) bookCategory = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
   if ("役職" == sheet) bookProfileDirector = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
   if ("グループ" == sheet) bookProfileGroup = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
 })
-let bookProfiles = bookProfileDirector.concat(bookProfileGroup) //各 Profile データを結合
 
-// 2. 項目名シートに倣って key 名を英語に変換
-let profileInEnglish = bookProfiles.map(profileData => {
+const bookProfiles = bookProfileDirector.concat(bookProfileGroup) //各 Profile データを結合
+
+// 2. データの一部を変換 (key:日本語表記 => 英語表記 / 改行文字 => HTML タグ)
+let keyEN
+
+const newBookProfiles = bookProfiles.map(profileData => {
   let newProfileData = {}
   for (const key in profileData) {
     if (Object.hasOwnProperty.call(profileData, key)) {
-      bookCategory.map(categoryData => {
-        if (categoryData.日本語 == key) {
-          const newKey = key.replace(key, categoryData.英語)
-          newProfileData[newKey] = profileData[key]
-        }
-      })
+      replaceKeyName(key) // key の英語表記を取得
+      newProfileData[keyEN] = profileData[key].replace(/[\n]/g, '<br>') // 改行文字を HTML タグに変換し代入
     }
   }
   return newProfileData
 })
 
-// 3．改行 \n を <br> に変換
-profileInEnglish.forEach((profileData) => {
-  for (const key in profileData) {
-    if (Object.hasOwnProperty.call(profileData, key)) {
-      const newElement = profileData[key].replace(/[\n]/g, '<br>')
-      profileData[key] = newElement
-    }
-  }
-})
+// 項目名シートに倣って、日本語表記の key を英語表記で返す
+function replaceKeyName(keyJP) {
+  bookCategory.map(categoryData => {
+    if (categoryData.日本語 == keyJP) keyEN = keyJP.replace(keyJP, categoryData.英語)
+    return keyEN
+  })
+}
 
-// 4. id を追加しグループ化
-const exportData = profileInEnglish.reduce((acc, obj) => {
-  let property = 'id'
-  let key = obj[property]
-  acc[key] = obj
-  delete obj[property]
-  return acc
-}, {})
+// 3. オブジェクト内の id を使って、各オブジェクトをグループ化
+const exportData = newBookProfiles.reduce((afterData, beforeData) => {
+  const targetKey = 'id'
+  let addGroupID = beforeData[targetKey] // id をグループ名にする
 
-// 5．出力
+  afterData[addGroupID] = beforeData // 各オブジェクトにグループ名を追加
+  delete beforeData[targetKey] // 各オブジェクトから id を削除
+  return afterData
+}, {}) // 配列をオブジェクトに変換
+
+// 4．出力
 fs.writeFileSync(`json/${fileName}.json`, JSON.stringify(exportData, null, 2));
